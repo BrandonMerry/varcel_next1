ececedae6d18cf2f16dc22c2134e0a5356e4c1be
brandon <brandon.l.merry@gmail.com>
2022-09-15 11:06:06 +0700

    added and removed srcipts component

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,20 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Script from 'next/script';
 
 export default function FirstPost() {
-  return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
-  );
-}
+    return (
+      <>
+        <Head>
+          <title>First Post</title>
+        </Head>
+       
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+      </>
+    );
+  }
